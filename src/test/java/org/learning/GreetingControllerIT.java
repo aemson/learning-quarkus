c90d48283b21fa2953a8d5673182634c9739766b
quarkusio <no-reply@quarkus.io>
2022-08-26 10:34:56 +0000

    Initial commit

---
+++ src/test/java/org/learning/GreetingControllerIT.java
@@ -0,0 +1,8 @@
+package org.learning;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingControllerIT extends GreetingControllerTest {
+    // Execute the same tests but in packaged mode.
+}
